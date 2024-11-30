b019d9f95c371155e60c6d0253a251866dfd2226
Admin <Admin@DESKTOP-UGP0C2G>
2024-11-30 10:06:29 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите целое число N (>2): ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N <= 2)
+{
+    Console.WriteLine("N должно быть больше 2!");
+    return;
+}
+Console.Write("Введите значение A: ");
+double A = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение B: ");
+double B = Convert.ToDouble(Console.ReadLine());
+if (A >= B)
+{
+    Console.WriteLine("A должно быть меньше B");
+    return;
+}
+double H = (B - A) / (N - 1);
+Console.WriteLine($"Значения функции F в {N} точках:");
+for (int i = 0; i < N; i++)
+{
+    double X = A + i * H;
+    double F_X = 1 - Math.Sin(X);
+    Console.WriteLine($"F({X}) = {F_X}");
+}
